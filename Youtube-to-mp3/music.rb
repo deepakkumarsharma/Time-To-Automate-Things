076ff2af163b61fdf34626478f851d304c22092f
deepakkumarsharma <deeky.sharma@gmail.com>
2015-12-06 19:14:29 +0530

    .tr(exec, system)

--- Youtube-to-mp3/music.rb
+++ Youtube-to-mp3/music.rb
@@ -29,38 +29,45 @@ print "Decision --> "
 user_audio_video_input = $stdin.gets.chomp
 av_input = user_audio_video_input.downcase
 
-=begin
-  Video Quality
-=end
-puts "-" * 26
-puts "-- Choose Video Quality --"
-puts "-" * 26
-puts "\n"
-print "Low | Medium | High | Best --> "
-user_quality_input = $stdin.gets.chomp
-quality_input = user_quality_input.downcase
 
 =begin
   Decision And Condition
 =end
 if (av_input == "video")
+=begin
+  Video Quality
+=end
+  puts "-" * 26
+  puts "-- Choose Video Quality --"
+  puts "-" * 26
+  puts "\n"
+  print "Low | Medium | High | Best --> "
+  user_quality_input = $stdin.gets.chomp
+  quality_input = user_quality_input.downcase
+=begin
+  Case According to Quality
+=end    
   case quality_input
     when "low"
-      exec 'youtube-dl --format 160 ' + get_user_input    
+      system 'youtube-dl --format 160 ' + get_user_input    
     when "medium"
-      exec 'youtube-dl --format 136 ' + get_user_input    
+      system 'youtube-dl --format 136 ' + get_user_input    
     when "high"
-      exec 'youtube-dl --format 137 ' + get_user_input      
+      system 'youtube-dl --format 137 ' + get_user_input      
     else
-      exec 'youtube-dl --format 22 ' + get_user_input  
+      system 'youtube-dl --format 22 ' + get_user_input  
   end  
+elsif (av_input == "audio")
+  system 'youtube-dl --extract-audio --audio-format mp3 -f bestaudio --audio-quality 0 ' + get_user_input  
 else
-  exec 'youtube-dl --extract-audio --audio-format mp3 -f bestaudio --audio-quality 0 ' + get_user_input  
+  puts "-------------- Wrong input ---------------"
+  puts "-- Please choose between Audio or Video --"
+  puts "-" * 42
 end
 
-=begin
-  Success Message
-=end
-puts "-" * 25
-puts "-- Download Completed -- "
-puts "-" * 25
+if $? == 0
+  puts "\n"
+  puts "-" * 25
+  puts "-- Download Completed --"
+  puts "-" * 25
+end
