e37a8c7acbc8a161e79efe65d8215f7cc09b2a62
deepakkumarsharma <deeky.sharma@gmail.com>
2015-12-11 21:16:24 +0530

    Deleted extra files

--- Youtube-to-mp3/music.sh
+++
@@ -1,7 +0,0 @@
-echo "Please enter youtube-video-link: "
-read input_music_link
-youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 $input_music_link
-
-echo "########################"
-echo "## Download Completed ##"
-echo "########################"
